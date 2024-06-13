4e6ba140303ceab7c1a53096ba76df87f5869e8f
rellenos <danieling00@gmail.com>
2024-06-13 12:20:23 +0200

    funciones de borrar y limpiar

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,39 @@
+const historial = [];
+// var pantalla = document.getElementById("pantalla");
+
 function asignarValor(caracter) {
-    let pantalla = document.getElementById("pantalla");
-    sessionStorage.setItem("resultat", pantalla.value);
-    pantalla.value += caracter;
+    let pantalla = document.getElementById("pantalla"); //assignar la variable "pantalla" a partir del element amb id "pantalla", en aquest cas un input-text que farà de panell de visualitzacio de la calculadora
+    sessionStorage.setItem("operations", pantalla.value); //
+    pantalla.value += caracter; //
 }
 
 function resolver() {
-    let pantalla = document.getElementById("pantalla");
-    let resultado = eval(pantalla.value);
-    sessionStorage.setItem("resultat", resultado);
-    pantalla.value = resultado;
+    try {
+        let pantalla = document.getElementById("pantalla");
+        let resultado = eval(pantalla.value);
+        let pantallaHistorial = document.getElementById("historial");
+        historial.push(" " + pantalla.value + "=" + resultado);
+        sessionStorage.setItem("operations", resultado);
+        pantalla.value = resultado;
+        pantallaHistorial.value = historial;
+    }
+    catch(error) {
+        document.getElementById("pantalla").value = 'Error';
+    }
 }
 
-function clear() {
+function retroceder() {
+    let pantalla = document.getElementById("pantalla");
+    pantalla.value = pantalla.value.slice(0, -1);
+}
+
+function limpiar() {
     let pantalla = document.getElementById("pantalla");
     pantalla.value = "";
-    sessionStorage.removeItem("resultat")
+    sessionStorage.removeItem("operations")
 }
+
+window.addEventListener("keydown",(e)=>{
+    if(e.keyCode===13) {resolver()}
+    if(e.keyCode===67) {limpiar()}
+}) 
